e1bfe468cf0ec58499219649bb6f3fa7e848e0d3
jjsss5848-hue <jjsss5848@gmail.com>
2026-01-19 10:12:57 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,45 @@
+// Service Worker pour Galerie PWA
+const CACHE_NAME = 'gallery-pwa-v3';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+];
+
+// Installation
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Cache ouvert');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Interception des requêtes
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Retourne du cache si disponible, sinon fetch
+        return response || fetch(event.request);
+      })
+  );
+});
+
+// Nettoyage des anciens caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Suppression de l\'ancien cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
